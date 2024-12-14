043eba6b351d4b4be879f904f1c84e9255f47a8d
sevenjune <yahiawissal76@gmail.com>
2024-12-14 21:44:33 +0100

    14 dec

--- 7j/frontend/src/components/ChatApp.jsx
+++ 7j/frontend/src/components/ChatApp.jsx
@@ -75,14 +75,8 @@ const ChatApp = () => {
   return (
       <body>
   <header className="app-header">
-      <h1>Welcome to My Chat App</h1>
-      <nav>
-        <ul className="header-nav">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
-      </ul>
-    </nav>
+      <button className="btheader"> 7 JUNE</button>
+      
   </header>
     
     <div className="app-container">
@@ -133,7 +127,7 @@ const ChatApp = () => {
       </div>
     </div>
     <footer className="app-footer">
-    <p>&copy; 2024 Your Company Name. All rights reserved.</p>
+    <p>&copy; 2025 seven june. All rights reserved.</p>
     <p>
       Follow us on 
       <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"> Facebook</a>, 
